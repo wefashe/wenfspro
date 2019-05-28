583567312c72ccaf0a13bebee6576c273e88ec99
wenfs <wenfs@yonyou.com>
2019-05-28 14:31:27 +0800

    添加vue-client项目

---
+++ vue-client/src/main.js
@@ -0,0 +1,23 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { postRequest } from "./utils/api"
+import { putRequest } from "./utils/api"
+import { deleteRequest } from "./utils/api"
+import { getRequest } from "./utils/api"
+Vue.prototype.getRequest = getRequest
+Vue.prototype.deleteRequest = deleteRequest
+Vue.prototype.putRequest = putRequest
+Vue.prototype.postRequest = postRequest
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
